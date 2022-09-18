030bfff53014d7af60ca37fe34bdd0b455723d0d
星光 <starshine@aili.moe>
2022-09-18 17:34:07 +0800

    perf: 清理无用代码

--- src/config/router.rs
+++ src/config/router.rs
@@ -18,20 +18,4 @@ pub fn router(config: &mut ServiceConfig) {
                     .service(user::get_current_user)
             )
     );
-    // .service(
-    //     scope("/api/v1/user")
-    //         // 身份验证中间件
-    //         // 不能写在 main 那里，那里会拦截全部请求
-    //         // 这里对此 scope 下的所有路由起作用
-    //         .wrap(HttpAuthentication::bearer(validator))
-    //         .service(get_all_users)
-    //         .service(get_user)
-    //         .service(add_new_user)
-    //         .service(update_user)
-    //         .service(del_user)
-    // );
-    // .service(
-    //     scope("/api/v1")
-    //         .service(login)
-    // );
 }
--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,4 +1,3 @@
 pub mod user;
-pub mod auth;
 
 pub use user::*;
